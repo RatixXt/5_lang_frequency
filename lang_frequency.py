234b2c2e511d4648e8689373fc4660d3a5d50d73
Shcherbakov Alexey <lovelessxt@gmail.com>
2016-10-11 15:44:01 +0300

    Добавлен скрипт для частотного анализа
    Добавлена функция get_most_frequent_words принимающая на вход текст в формате string и возвращающая 10 самых встречаемых слов текста и количество их вхождений.
    
    Добавлена функция get_filepath, позволяющая пользователю ввести путь к файлу с текстом.
    
    Добавлена функция load_data принимающая на вход файл текстового формата в кодировке utf-8 и возвращающая данные в формате string.

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,30 @@
+# -*- coding: utf-8 -*-
+from os.path import exists
+from re import findall
+from collections import Counter
+from codecs import open
 
 
 def load_data(filepath):
-    pass
+    if exists(filepath):
+        with open(filepath, 'r', 'utf-8') as file_handler:
+            return file_handler.read()
 
 
 def get_most_frequent_words(text):
-    pass
+    words = findall(r'[a-я]\w+|[a-z]\w+', text.lower())
+    return Counter(words).most_common(10)
+
+
+def get_filepath():
+    return input(u"Введите путь к файлу:")
 
 
 if __name__ == '__main__':
-    pass
+    filepath = get_filepath()
+    data = load_data(filepath)
+    if data is not None:
+        common_words = get_most_frequent_words(data)
+        print(common_words)
+    else:
+        print(u'Данных нет или указан неверный путь к файлу')
